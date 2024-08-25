4ac474fc896839941bdaa6b2e58753d19162668f
mustyverymustymustapha <mustaphasatti1@gmail.com>
2024-08-25 05:55:57 +0000

    kwiirjf

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ function getRandomBookText() {
         .then(data => {
             if (data.description) {
                 let text = typeof data.description === 'object' ? data.description.value : data.description;
+                text = text.split(' ').slice(0, 50).join(' ') + '...';
+                textElement.textContent = text;
+                startFlipping();
+            } else {
+                getRandomBookText();
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            getRandomBookText();
+        });
+}
+
+function startFlipping() {
+    setInterval(() => {
+        textElement.classList.toggle('upside-down');
+    }, 3000);
+}
+
+getRandomBookText();
